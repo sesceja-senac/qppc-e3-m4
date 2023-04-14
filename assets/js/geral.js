d149cd7eb173adec803ae35c2bea642a1f81ba4d
luiz-brancher <lppbrancher@senacrs.com.br>
2023-04-14 03:44:12 -0300

    tela inicial e boa noite

--- assets/js/geral.js
+++ assets/js/geral.js
@@ -3,14 +3,6 @@ window.addEventListener('load', function(){
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
       return new bootstrap.Tooltip(tooltipTriggerEl)
     })
-    AOS.refresh()
+    
     $('.preloader').fadeOut(300);
-})
-
-$('.cartaMemoria').click(function(){
-  if($(this).hasClass('flip')){
-      $(this).removeClass('flip')
-  }else{
-      $(this).addClass('flip')
-  }
 })
